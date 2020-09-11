a64377ff44a4f7e435603a0b0f9a7b71eb961615
mattsmvs <69328968+mattsmvs@users.noreply.github.com>
2020-09-11 10:38:12 -0700

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'hU4qiUVlvDFCpOb1yoP-Uw5eq39LZy2peAZOVvCUq_-k0VWNhXuRIlpVDc3ytM2PVSNlIkA025pLuHxSwMJikYqbNcqHOqqowDQwiWqdEN72BYeu324nrJdaDXr6XXYx';
+const Yelp = {
+    searchYelp(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if(jsonResponse.businesses){
+                return jsonResponse.businesses.map((business) => { 
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                    };
+                });
+            }
+        })
+    }
+};
+
+export default Yelp;
+
